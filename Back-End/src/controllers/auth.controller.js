f971a1c1e2b11f384a3e1ea1d002056230a20b72
Alejjaandro <alejandroolaso98@gmail.com>
2023-07-21 15:03:07 +0200

    Token and hash

--- Back-End/src/controllers/auth.controller.js
+++ Back-End/src/controllers/auth.controller.js
@@ -1,29 +1,47 @@
-// Importing User model
+// Importing User model.
 import User from '../models/userModel.js';
+// Importing bcrpt for crypting.
+import bcrypt from 'bcryptjs';
+// Importing function to create a token.
+import {createAccessToken} from '../libs/jwt.js';
 
 // Creating the functions that will handle the info of the routes:
 
 export const register = async (req, res) => {
 
-    // Extractinf the info from the body.
+    // Extract the info from the body.
     const {email, password, username} = req.body;
 
     try {
+        // Crypting password.
+        const hashPassword = await bcrypt.hash(password, 10);
         // Creating a new user with the info.
         const newUser = new User({
             username,
             email,
-            password
+            password: hashPassword
         });
 
         // Registering the new user in the database.
-        await newUser.save();
+        const userSaved = await newUser.save();
 
+        // Create Token (created in libs/jwt.js).
+        const token = await createAccessToken( {id: userSaved._id} );
+            
+        // We save the token in a cookie and send a success message.
+        res.cookie('token', token);
 
-        res.send('Registered');
+        // Send a response with the new User to the client.
+        res.json({
+            id: newUser.id,
+            username: newUser.username,
+            email: newUser.email,
+            createdAt: newUser.createdAt,
+            updatedAt: newUser.updatedAt
+        });
 
     } catch (error) {
-        console.log(error);
+        res.status(500).json({message: error.message});
     }
 };
 
